9e40788182967f76851b7819e6d80471567dd7e4
Roman Raßloff <retch100@gmail.com>
2023-10-20 12:12:24 +0200

    add types

---
+++ src/app/types/AddLockType.ts
@@ -0,0 +1,7 @@
+export interface AddLockType {
+  description: string;
+  batteryVoltageMin: number;
+  batteryVoltageMax: number;
+  cellularSignalQualityMin: number;
+  cellularSignalQualityMax: number;
+}
---
+++ src/app/types/UpdateLockType.ts
@@ -0,0 +1,8 @@
+export interface UpdateLockType {
+  id: number;
+  description: string;
+  batteryVoltageMin: number;
+  batteryVoltageMax: number;
+  cellularSignalQualityMin: number;
+  cellularSignalQualityMax: number;
+}
